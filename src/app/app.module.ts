78ee44e960bf691594c86a6df0d7a0c23129f914
Juan Carlos Muro <juancarlos.muro@valtech.nl>
2019-07-30 15:32:43 +0200

    Added the 'observable1' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Observable1Component } from './observable1/observable1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Observable1Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/observable1/observable1.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import { noop, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-observable1',
+  //templateUrl: './observable1.component.html',
+  //styleUrls: ['./observable1.component.css']
+  template: `
+  <b>Angular Component Using Observables! </b>
+  <button style="margin-top: 2rem;" (click)="launch()">Launch</button>
+  <button style="margin-top: 2rem;" (click)="alternate()">Alternate</button>
+  <button style="margin-top: 2rem;" (click)="stop()">Stop</button>
+
+  <div>VALUES:   <span *ngFor="let value of values">{{ value }} </span></div>
+  <div>ERRORs:   <span>{{ errors }}</span></div>
+  <div>FINISHED: <span>{{ finished }}</span></div>
+`
+})
+export class Observable1Component {
+  private values: Array<number> = [];
+  private errors: Error;
+  private finished: boolean;
+
+  private observable: Observable<number>;
+
+  private subscription: Subscription;
+
+  constructor() {
+    this.observable = new Observable(observer => {
+      let timeouts = [];
+      timeouts.push(setTimeout( () => {observer.next(42);},   1000 ));  // 
+      timeouts.push(setTimeout( () => {observer.next(43);},   2000 ));
+      timeouts.push(setTimeout( () => {Math.round(Math.random())?observer.error(new Error("Can't take this BS")):noop}, 1500 ));
+      timeouts.push(setTimeout( () => {observer.complete();}, 3000 ));
+
+      return () => { 
+        for (let timeout of timeouts) {
+          console.log("Removing timeout: " + timeout);
+          clearTimeout(timeout);
+        }
+      }
+    });
+  }
+
+  launch() {
+    this.values = []
+    this.errors = null
+    this.finished = false
+
+    //let subscription = this.observable.subscribe(
+    this.subscription = this.observable.subscribe(
+        val => this.values.push(val),
+        err => this.errors = err,
+        ()  => this.finished = true
+    );
+  }
+
+  stop() {
+    this.subscription.unsubscribe();
+  }
+
+  alternate() {
+    this.values = []
+    this.errors = null
+    this.finished = false
+
+    let promise = this.observable
+      .forEach(v => this.values.push(v))
+      .then(() => this.finished = true);
+  }
+}
